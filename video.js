fbfd6df8554eddada83a5cc40ef0a09db536e541
Evan <107043407+Evanc-1010@users.noreply.github.com>
2022-11-15 16:37:49 +0800

    update v2

--- video.js
+++ video.js
@@ -20,22 +20,18 @@ playbtn.addEventListener("click", () => {
 
 const recommandImgAll = document.querySelectorAll(".recommandVDO");
 
-recommandImgAll.forEach((recommandImg, index) => {
+recommandImgAll.forEach((recommandImg) => {
     recommandImg.addEventListener("click", clickEffect = () => {
-        switch (recommandImg){
-            case recommandImg[0]:
-            contentContainer.innerHTML = '',
-            contentContainer.innerHTML = '<iframe width="307" height="173" src="https://www.youtube-nocookie.com/embed/kVxTrhojpFI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-            break;
 
-            case recommandImg[1]:
-            contentContainer.innerHTML = '<iframe width="307" height="173" src="https://www.youtube-nocookie.com/embed/Bxo2JkiqG_o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-            break;
-
-            case recommandImg[2]:
-            contentContainer.innerHTML = '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/zleIaEIBs2M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-            break;
-            
+        if (recommandImg.classList.contains("recommandVDO_1")) {
+            contentContainer.innerHTML = '<iframe width="940" height="528" src="https://www.youtube-nocookie.com/embed/kVxTrhojpFI?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        } else if (recommandImg.classList.contains("recommandVDO_2")) {
+            contentContainer.innerHTML = '<iframe width="940" height="528" src="https://www.youtube-nocookie.com/embed/Bxo2JkiqG_o?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        } else if (recommandImg.classList.contains("recommandVDO_3")) {
+            contentContainer.innerHTML = '<iframe width="940" height="528" src="https://www.youtube-nocookie.com/embed/zleIaEIBs2M?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
         }
     })
 })
+
+/* click img to replace video in center --- */
+
