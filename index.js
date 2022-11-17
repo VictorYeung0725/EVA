48841570e484054312cd1ff94329ba8ac3f29272
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2022-11-17 21:16:34 +0800

    connect with evan part

--- index.js
+++ index.js
@@ -15,3 +15,20 @@ document.addEventListener('DOMContentLoaded', function (e) {
     }
   };
 });
+
+/* For hover / click to display nav items: */
+
+const navbar = document.querySelector('.navbar');
+const navlist = document.querySelectorAll('.navbar-nav');
+
+navbar.addEventListener('mouseover', () => {
+  navlist.forEach((nav) => {
+    nav.classList.remove('d-none');
+  });
+});
+
+navbar.addEventListener('mouseout', () => {
+  navlist.forEach((nav) => {
+    nav.classList.add('d-none');
+  });
+});
