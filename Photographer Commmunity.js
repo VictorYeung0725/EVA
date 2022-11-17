77a8616edfb99cbb59bdcb89bfb69f9275be7bb7
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2022-11-17 21:23:44 +0800

    link with community page

--- Photographer Commmunity.js
+++ Photographer Commmunity.js
@@ -16,3 +16,20 @@ add.forEach((ad) => {
     }
   });
 });
+
+/* For hover / click to display nav items: */
+
+const navbar = document.querySelector('.navbar');
+const navlist = document.querySelectorAll('.navbar-nav');
+
+navbar.addEventListener('mouseover', () => {
+  navlist.forEach((nav) => {
+    nav.classList.remove('d-none');
+  });
+});
+
+navbar.addEventListener('mouseout', () => {
+  navlist.forEach((nav) => {
+    nav.classList.add('d-none');
+  });
+});
