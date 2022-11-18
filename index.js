96cdb517f89a33ac13ff0db13551a96398ef1d45
Evan <107043407+Evanc-1010@users.noreply.github.com>
2022-11-18 21:50:00 +0800

    Added cursor effect

--- index.js
+++ index.js
@@ -32,3 +32,31 @@ navbar.addEventListener('mouseout', () => {
     nav.classList.add('d-none');
   });
 });
+
+
+/* --------------------------For cursor to display: */
+
+let mouseCursor = document.querySelector(".cursor");
+// let navLinks = document.querySelectorAll(".navbar-nav li");
+
+const cursor = (e)=> {
+  const mouseY = e.pageY + 30;
+  const mouseX = e.pageX + 20;
+
+  mouseCursor.style.top = `${mouseY}px`;
+  mouseCursor.style.left = `${mouseX}px`;
+}
+
+window.addEventListener("mousemove", cursor);
+
+
+/* ------------------------For cursor change on active: */
+
+
+window.addEventListener("mousedown", () => {
+  mouseCursor.innerHTML = '<img src="./img/camera2.png" alt="" />';
+});
+
+window.addEventListener("mouseup", () => {
+  mouseCursor.innerHTML = '<img src="./img/camera.png" alt="" />';
+});
--- video.js
+++ video.js
@@ -75,3 +75,31 @@ navbar.addEventListener("mouseout", () => {
 })
 
 /* ------------------------------------------- */
+
+
+/* --------------------------For cursor to display: */
+
+let mouseCursor = document.querySelector(".cursor");
+// let navLinks = document.querySelectorAll(".navbar-nav li");
+
+const cursor = (e)=> {
+  const mouseY = e.pageY + 30;
+  const mouseX = e.pageX + 20;
+
+  mouseCursor.style.top = `${mouseY}px`;
+  mouseCursor.style.left = `${mouseX}px`;
+}
+
+window.addEventListener("mousemove", cursor);
+
+
+/* ------------------------For cursor change on active: */
+
+
+window.addEventListener("mousedown", () => {
+  mouseCursor.innerHTML = '<img src="./img/camera2.png" alt="" />';
+});
+
+window.addEventListener("mouseup", () => {
+  mouseCursor.innerHTML = '<img src="./img/camera.png" alt="" />';
+});
